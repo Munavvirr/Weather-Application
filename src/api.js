5c3e4d9ec96956c101e9f52d978065bbaa9a3bfd
Munavvirr <72682848+Munavvirr@users.noreply.github.com>
2023-08-19 16:32:10 +0400

    weather-app

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a0d49a065fmsh7865e6e24e1a019p11f23djsn23a672bacf02',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,21 @@
+import "./current-weather.css"
+
+const CurrentWeather = () => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city">Belgrade</p>
+                    <p className="weather-description">Sunny</p>
+                </div>
+                <img alt="weather" className="weather-icon" src="icons/01d.png"></img>
+            </div>  
+
+            <div className="bottom">
+                <p className="temperature">18C</p>
+            </div> 
+        </div>
+    )
+}
+
+export default CurrentWeather
