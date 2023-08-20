380b02d176c2dd09cd09ef71d50e5cec75d57d65
Munavvirr <72682848+Munavvirr@users.noreply.github.com>
2023-08-20 19:50:10 +0400

    Weather application

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a0d49a065fmsh7865e6e24e1a019p11f23djsn23a672bacf02',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
 };
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const wEATHER_API_KEY = "8362f133a8c6ecb5e14fb0c290e089dc"
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
-import { AsyncPaginate } from "react-select-async-paginate";
-import { geoApiOptions, GEO_API_URL } from "../../api";
+import {AsyncPaginate} from "react-select-async-paginate";
+import { useState } from "react";
+import { GEO_API_URL, geoApiOptions} from "../../api";
+const Search = ({onSearchChange}) => {
 
-const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
+    const [search, setSearch] = useState(null)
 
-  const loadOptions = (inputValue) => {
-    return fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-      geoApiOptions
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response.data.map((city) => {
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+        .then(response=>response.json())
+        .then(response=>{
             return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name}, ${city.countryCode}`,
-            };
-          }),
-        };
-      });
-  };
+                options: response.data.map((city)=>{
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`
+                    }
+                })
+            }
+        }
+            )
+        .catch(err=>console.log(err))
+    }
 
-  const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
 
-  return (
-    <AsyncPaginate
-      placeholder="Search for city"
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
-  );
-};
-
-export default Search;
+    return (
+        <AsyncPaginate
+        placeholder = "Search for city"
+        debounceTimeout={600}
+        value = {search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+        />
+    )
+}
+export default Search
